21bc1e4c9b3294edf0b35a44662e6050a9c9264a
Benoit Anctil-Robitaille <benoit.robitaille@hotmail.ca>
2017-03-22 22:41:22 +0100

    + Added searching image, sensor query in views, modified some css

--- sensors/views.py
+++ sensors/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from rest_framework.decorators import api_view
+from sensors.models import Sensor
 
 
 @api_view(['GET'])
 def index(request):
     if request.method == 'GET':
-        return render_to_response('sensors/sensors-index.html', {'content_title': "Sensors"}, RequestContext(request))
+        context = {
+            'content_title': "Sensors",
+            'sensors': Sensor.objects.all(),
+        }
+        return render_to_response('sensors/sensors-index.html', context, RequestContext(request))
