5317be8176b94f3846991748685da93acac5b209
Benoit Anctil-Robitaille <benoit.robitaille@hotmail.ca>
2017-03-27 00:06:15 +0200

    + Added error and success message on dashboard creation

--- dashboards/static/js/dashboards.js
+++ dashboards/static/js/dashboards.js
@@ -3,23 +3,20 @@
  */
 $(document).ready(function () {
     $('#createDashboardButton').on('click', function (event) {
-        event.preventDefault();
         var dashboardName = $("#dashboardName").val();
         var dashboardDescription = $("#dashboardDescription").val();
         var dashboardTemplate = $("#dashboardTemplate").val();
         var queryParams = "?name=" + dashboardName + "&description=" + dashboardDescription + "&template=" + dashboardTemplate;
         if (dashboardName) {
-            alert(dashboardName);
             $("#dashboardNameError").hide();
             jQuery.ajax({
                 url: "http://localhost:8000/dashboards" + queryParams,
                 type: "POST",
                 success: function (resultData) {
-
+                    swal("Great", "Dashboard created successfully", "success")
                 },
                 error: function (jqXHR, textStatus, errorThrown) {
-
-                    console.log(errorThrown);
+                    sweetAlert("Oops...", "An error has occurred while creating the dashboard", "error");
                 },
 
                 timeout: 12000000,
