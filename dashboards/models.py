fde83416cd8ae4eac9709ecca7153566f0287eae
Benoit Anctil-Robitaille <benoit.robitaille@hotmail.ca>
2017-04-13 10:39:06 -0500

    + Added dashboard model field to track size, measure unit, grid position , etc

--- dashboards/models.py
+++ dashboards/models.py
@@ -9,9 +9,16 @@ class DashboardTemplates(object):
 
 
 class DashboardWidget(EmbeddedDocument):
+    TYPE = ["line-chart", "altitude-gauge", "vertical-speed-gauge", "heading-gauge", "air-speed-gauge"]
+
     uuid = UUIDField(required=True)
+    sensor_id = UUIDField()
     name = StringField(required=True)
     description = StringField()
+    type = StringField()
+    measure_units = StringField()
+    grid_position = IntField(min_value=1)
+    size = IntField(min_value=1, max_value=5)
 
 
 class Dashboard(Document):
